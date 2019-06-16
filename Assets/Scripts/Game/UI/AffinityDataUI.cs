dbb455a8d4f49a1707c3ce6f7b8d65eb9b4c309d
Belwyn <ismaelmgr@gmail.com>
2019-06-17 01:05:12 +0200

    Prefab for UI data value representation and Affinity structure

---
+++ Assets/Scripts/Game/UI/AffinityDataUI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Vagrant.Game.Core;
+
+namespace Vagrant.Game.UI {
+
+    public class AffinityDataUI : MonoBehaviour {
+
+        AffinityData _data;
+        public AffinityData data {
+            set {
+                _data = value;
+                UpdateData();
+            }
+        }
+
+        List<DataValueUI> _uiValues;
+
+
+        private void Awake() {
+
+            _uiValues = new List<DataValueUI>(GetComponentsInChildren<DataValueUI>());
+            int i = 0;
+
+            foreach (DataValueUI dataValueUI in _uiValues){
+                dataValueUI.label = ((AffinityEnum)i).ToString();
+                i++;
+            }
+
+        }
+
+
+        // DELETE ME
+        /*
+        private void Update() {
+            if (_model == null) {
+                Blade blade = FindObjectOfType<Blade>();
+                if (blade != null)
+                    _model = blade.affinityData;
+            } 
+            else UpdateData();
+
+        }
+        */
+
+        private void UpdateData() {
+            int i = 0;
+            foreach (DataValueUI dataValueUI in _uiValues) { 
+                dataValueUI.value = _data[i];
+                dataValueUI.barFill = _data[i] * 0.1f;
+                i++;
+            }
+
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Game/UI/DataValueUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataValueUI : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text _label;
+
+    [SerializeField]
+    private Text _value;
+
+    [SerializeField]
+    private Image _bar;
+
+
+
+    public string label {
+        get { return _label?.text; }
+        set { _label.text = value; }
+    }
+
+    public int value {
+        get { return int.Parse(_value?.text); }
+        set { _value.text = value.ToString(); }
+    }
+
+    public float barFill {
+        set { _bar.fillAmount = value; }
+    }
+
+}
